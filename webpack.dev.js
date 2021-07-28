400cf6ea9f0c31de1ed8da42e77b9a4762bd0891
harry <1290334299@qq.com>
2021-07-28 16:03:57 +0800

    feat: 新增public等

--- webpack.dev.js
+++ webpack.dev.js
@@ -9,18 +9,17 @@ const path = require('path');
 const fs = require('fs');
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CreateReactPublicPlugin = require('create-react-public-plugin');
-const InlineChunkHtmlPlugin = require('react-dev-utils/InlineChunkHtmlPlugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const InterpolateHtmlPlugin = require('react-dev-utils/InterpolateHtmlPlugin');
 const getPublicUrlOrPath = require('react-dev-utils/getPublicUrlOrPath');
+
+
 
 // Make sure any symlinks in the project folder are resolved:
 // https://github.com/facebook/create-react-app/issues/637
 const appDirectory = fs.realpathSync(process.cwd());
 const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
-
 // We use `PUBLIC_URL` environment variable or "homepage" field to infer
 // "public path" at which the app is served.
 // webpack needs to know it to put the right <script> hrefs into HTML even in
@@ -32,16 +31,69 @@ const publicUrlOrPath = getPublicUrlOrPath(
   require(resolveApp('package.json')).homepage,
   process.env.PUBLIC_URL
 );
+const buildPath = process.env.BUILD_PATH || 'build';
+const moduleFileExtensions = [
+  'web.mjs',
+  'mjs',
+  'web.js',
+  'js',
+  'web.ts',
+  'ts',
+  'web.tsx',
+  'tsx',
+  'json',
+  'web.jsx',
+  'jsx',
+];
+// Resolve file paths in the same order as webpack
+const resolveModule = (resolveFn, filePath) => {
+  const extension = moduleFileExtensions.find(extension =>
+    fs.existsSync(resolveFn(`${filePath}.${extension}`))
+  );
+  if (extension) {
+    return resolveFn(`${filePath}.${extension}`);
+  }
+  return resolveFn(`${filePath}.js`);
+};
+const paths = {
+  dotenv: resolveApp('.env'),
+  appPath: resolveApp('.'),
+  appBuild: resolveApp(buildPath),
+  appPublic: resolveApp('public'),
+  appHtml: resolveApp('public/index.html'),
+  appIndexJs: resolveModule(resolveApp, 'src/index'),
+  appPackageJson: resolveApp('package.json'),
+  appSrc: resolveApp('src'),
+  appTsConfig: resolveApp('tsconfig.json'),
+  appJsConfig: resolveApp('jsconfig.json'),
+  yarnLockFile: resolveApp('yarn.lock'),
+  testsSetup: resolveModule(resolveApp, 'src/setupTests'),
+  proxySetup: resolveApp('src/setupProxy.js'),
+  appNodeModules: resolveApp('node_modules'),
+  swSrc: resolveModule(resolveApp, 'src/service-worker'),
+  publicUrlOrPath,
+};
 
-const publicUrl = publicUrlOrPath;
+
+
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
+    // contentBase: paths.appPublic,
+    // contentBasePublicPath: paths.publicUrlOrPath,
+    publicPath: paths.publicUrlOrPath.slice(0, -1),
     port: 9000,
+    historyApiFallback: {
+      disableDotRule: true,
+      index: paths.publicUrlOrPath,
+    },
   },
   output: {
-    publicPath: publicUrl,
+    // webpack uses `publicPath` to determine where the app is being served from.
+    // It requires a trailing slash, or the file assets will get an incorrect path.
+    // We inferred the "public path" (such as / or /my-project) from homepage.
+    publicPath: paths.publicUrlOrPath,
     filename: "js/[name].js",
     globalObject: 'this',
   },
@@ -52,18 +104,20 @@ module.exports = merge(common, {
     // 生成 index.html
     new HtmlWebpackPlugin({
       inject: true,
-      template: path.resolve('public/index.html'),
+      // template: path.resolve('public/index.html'),
+      template: paths.appHtml,
     }),
-    new InlineChunkHtmlPlugin(HtmlWebpackPlugin, [/runtime-.+[.]js/]),
     // Makes the public URL available as %PUBLIC_URL% in index.html, e.g.:
     // <link rel="icon" href="%PUBLIC_URL%/favicon.ico">
     new InterpolateHtmlPlugin(HtmlWebpackPlugin, {
-      PUBLIC_URL: publicUrl,
+      PUBLIC_URL: publicUrlOrPath+'public',
       // You can pass any key-value pairs, this was just an example.
       // WHATEVER: 42 will replace %WHATEVER% with 42 in index.html.
     }),
-    // new CleanWebpackPlugin(),
-    new CreateReactPublicPlugin(__dirname,'public',['favicon.ico','index.html','logo192.png','logo512.png','manifest.json','robots.txt']),
+    new CreateReactPublicPlugin({
+      rootPath: __dirname,
+      projectName: 'public',
+      outputFiles: ['favicon.ico','index.html','logo192.png','logo512.png','manifest.json','robots.txt'],
+    }),
   ],
-  
 });
